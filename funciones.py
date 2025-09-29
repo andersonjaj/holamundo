1222a4fc0ac1bb30854ec5d1b326bf3bbf35b994
andersonjaj <andersonjaj1912@gmail.com>
2025-09-28 20:59:24 -0400

    clases   46 y 47

---
+++ funciones.py
@@ -0,0 +1,16 @@
+def mifuncion ():
+    print("@mi primera funcion")
+
+#mifuncion()
+
+#def imprimirdato (argumento1):
+ #   print("mi argumento es : " , argumento1)
+
+#imprimirdato("parametro1" )
+
+def imprimirdato (nombre, apellido, edad):
+    print("mi nombre es : " , nombre)
+    print("mi apellido es : " , apellido)
+    print("mi edad es : " , edad)
+
+imprimirdato("Junior", "Petit", 33 )
